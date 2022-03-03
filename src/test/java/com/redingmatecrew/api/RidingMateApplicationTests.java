7fce329b5fef08b82e097603a42aa6d453215427
kaaang <kaaang@kakao.com>
2022-03-03 14:41:31 +0900

    Initial commit

---
+++ src/test/java/com/redingmatecrew/api/RidingMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redingmatecrew.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidingMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
