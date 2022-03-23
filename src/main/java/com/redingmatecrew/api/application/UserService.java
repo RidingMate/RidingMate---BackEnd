544b98f382828d11a7b48a2cbf2db4f99a4f2c7f
proceane <proceane1@gmail.com>
2022-03-23 14:34:20 +0900

    서비스에 있는 도메인 코드 주석처리

--- src/main/java/com/redingmatecrew/api/application/UserService.java
+++ src/main/java/com/redingmatecrew/api/application/UserService.java
@@ -16,9 +16,10 @@ public class UserService {
 
     // TODO : 회원 가입 완료 후 객체 반환, 회원가입 필요정보 파라미터 추가
     public void insertUser() {
+        // TODO : 도메인 완성 후에 서비스 구현
         // TODO : save 전에 비밀번호 암호화하기
-        User user = new User("userId", "passowrd", "username", UserRole.ROLE_USER);
-        userRepository.save(user);
+//        User user = new User("userId", "passowrd", "username", UserRole.ROLE_USER);
+//        userRepository.save(user);
     }
 
     // TODO: 2022-03-07 회원 정보 조회 구현 필요
